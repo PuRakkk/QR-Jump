f58c02791e566acf6fd97191e1f568f3df46bf34
PuRakkk <sarakchon11@gmail.com>
2025-02-17 12:02:57 +0700

    Your commit message

--- qr/qr/settings.py
+++ qr/qr/settings.py
@@ -36,10 +36,10 @@ SECRET_KEY = 'django-insecure-^11zc703ec=i%zisn(bnnc483f362)x_6-ztxjam&neo3c_b42
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["da9b-167-179-41-221.ngrok-free.app", "127.0.0.1"]
+ALLOWED_HOSTS = ["b070-110-235-223-133.ngrok-free.app", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://da9b-167-179-41-221.ngrok-free.app',  # Add all active ngrok URLs
+    'https://b070-110-235-223-133.ngrok-free.app',  # Add all active ngrok URLs
 ]
 
 
