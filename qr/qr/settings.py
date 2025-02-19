7ae6511079824e61d4dea027e4473eef688dfcf8
PuRakkk <sarakchon11@gmail.com>
2025-02-19 18:01:41 +0700

    Models and Database

--- qr/qr/settings.py
+++ qr/qr/settings.py
@@ -165,6 +165,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
